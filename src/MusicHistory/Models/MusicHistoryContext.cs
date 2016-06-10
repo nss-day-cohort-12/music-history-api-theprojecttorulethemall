0cb31e6666294a846f22d7812a5e555acd618acd
Ahmed Ansari <ahmedansari153@gmail.com>
2016-06-10 10:23:21 -0500

    Created VisualStudio project, created models, configured starup.cs and installed entity

---
+++ src/MusicHistory/Models/MusicHistoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicHistory.Models
+{
+    public class MusicHistoryContext : DbContext
+    {
+        public MusicHistoryContext(DbContextOptions<MusicHistoryContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Album> Album { get; set; }
+        public DbSet<Track> Track { get; set; }
+    }
+}
